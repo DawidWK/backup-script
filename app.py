ff7ad378f69955ae9cca5b152d3016489ef6d20b
Dawid <praca.dawidwk@gmail.com>
2022-07-16 14:03:49 +0200

    update: added dotenv file with secrets

--- app.py
+++ app.py
@@ -7,7 +7,5 @@ load_dotenv()
 if __name__ == "__main__":
     FOLDER_ID = os.getenv('FOLDER_ID')
     FILE_PATH = os.getenv('FILE_PATH')
-    # FOLDER_ID = '1RXkq5-CkGNiKLeYmdbc3GHGjgVs7qGFD'
-    # FILE_PATH = '/mnt/c/Users/bazzu/Documents/Backup/Passwords.kdbx'
 
     make_backup(FOLDER_ID, FILE_PATH)
